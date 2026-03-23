3d106423a7220ccad1f2c3b21a2d4448a5098494
André Gouveia de Lima <andregouveiadelima@gmail.com>
2026-03-22 22:10:33 -0300

    Update CartContext.js
    Criação do CartContext para gerenciar o carrinho e adicionada função addToCart.

--- meu-primeiro-app/app/CartContext.js
+++ meu-primeiro-app/app/CartContext.js
@@ -1,1 +1,21 @@
+import React, { createContext, useState } from 'react';
 
+//Criação do contexto
+export const CartContext = createContext();
+export function CartProvider({ children }) {
+  const [card, setCart] = useState([]);
+
+//Função para adição dos itens
+const addToCart = (item) => {
+  setCart([...cart, item]);
+  alert(`${item.nome} adicionado ao carrinho!`);
+};
+
+const total = cart.reduce((soma, item)) => soma + item.preco, 0);
+
+return (
+  <cartContext.Provider value={{ cart, addToCart, total }}>
+      {children}
+    </CartContext.Provider>
+  );
+}
