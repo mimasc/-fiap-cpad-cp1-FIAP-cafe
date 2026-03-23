047d02e0bd7284e56757d6de73a3de8e19865922
mimasc <mirella.mascarenhas1@gmail.com>
2026-03-23 11:48:25 -0300

    Refactor CartContext for clarity and functionality

--- meu-primeiro-app/app/CartContext.js
+++ meu-primeiro-app/app/CartContext.js
@@ -1,20 +1,22 @@
 import React, { createContext, useState } from 'react';
-
-//Criação do contexto
+ 
+// Criando o contexto
 export const CartContext = createContext();
+ 
 export function CartProvider({ children }) {
-  const [card, setCart] = useState([]);
-
-//Função para adição dos itens
-const addToCart = (item) => {
-  setCart([...cart, item]);
-  alert(`${item.nome} adicionado ao carrinho!`);
-};
-
-const total = cart.reduce((soma, item)) => soma + item.preco, 0);
-
-return (
-  <cartContext.Provider value={{ cart, addToCart, total }}>
+  const [cart, setCart] = useState([]);
+ 
+  // Função para adicionar itens
+  const addToCart = (item) => {
+    setCart([...cart, item]);
+    alert(`${item.nome} adicionado ao carrinho!`);
+  };
+ 
+  // Calcula o valor total automaticamente
+  const total = cart.reduce((soma, item) => soma + item.preco, 0);
+ 
+  return (
+    <CartContext.Provider value={{ cart, addToCart, total }}>
       {children}
     </CartContext.Provider>
   );
